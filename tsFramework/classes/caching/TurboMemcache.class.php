c15be17575835cc410c7cd4b0f030ef7d1fa5c8a
TiMESPLiNTER <github@timesplinter.ch>
2012-08-14 19:34:57 +0200

    first realy clean commit

---
+++ tsFramework/classes/caching/TurboMemcache.class.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * Description of MemCache
+ *
+ * @author Pascal Münst
+ * @copyright Copyright (c) 2012, Metanet AG
+ * @version 1.0
+ */
+class TurboMemcache extends TurboCache {
+	public function close() {
+		
+	}
+
+	public function getValue($key) {
+		
+	}
+
+	public function init() {
+		
+	}
+
+	public function storeValue($key, $value) {
+		
+	}
+}
+
+?>
