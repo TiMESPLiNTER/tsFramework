15e42357abf684e8080c926d7c0be8538b7df6de
github@timesplinter.ch <22monde+**>
2015-03-05 20:18:28 +0100

    Fixing git error

---
+++ src/ch/timesplinter/template/TemplateParseException.php
@@ -0,0 +1,13 @@
+<?php
+
+
+namespace ch\timesplinter\template;
+
+
+/**
+ * @autor Pascal Muenst <dev@timesplinter.ch>
+ * @copyright Copyright (c) 2015 by TiMESPLiNTER Webdevelopment
+ */
+class TemplateParseException {
+
+}
